e53c658eebc6f6b173c5e4d6051934fb39a16b1a
saipratap99 <122005067@sastra.ac.in>
2020-03-05 16:57:30 +0530

    Added ability to create a new todo and add to DB in todos/create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,11 @@ class TodosController < ApplicationController
     id = params[:id]
     render plain: Todo.find(id).to_pleasant_string
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = Date.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    render plain: "Your newly created todo id is #{new_todo.id}"
+  end
 end
