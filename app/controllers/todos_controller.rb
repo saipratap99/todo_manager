066f74be7313ede788fdd5a79bdb3e6ae829ca88
saipratap99 <122005067@sastra.ac.in>
2020-03-05 16:48:29 +0530

    Rendening todo with todos/;id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,9 @@ class TodosController < ApplicationController
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
+
+  def show
+    id = params[:id]
+    render plain: Todo.find(id).to_pleasant_string
+  end
 end
