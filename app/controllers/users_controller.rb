57ebfa82e0bc2520528d39b8621019740782ac62
saipratap99 <122005067@sastra.ac.in>
2020-03-06 14:14:01 +0530

    Create a user by POST and show user by users/:id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
   def index
     render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
   end
+
+  def show
+    id = params[:id]
+    render plain: User.find(id).to_pleasant_string
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    render plain: "Id of new user is #{new_user[:id]}"
+  end
 end
