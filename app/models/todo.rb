4350bc41f20eb78d76fbf6f9839d219df08a7759
saipratap99 <122005067@sastra.ac.in>
2020-03-25 15:08:19 +0530

    Rendering Dyanamic webpage using ERB templating and render todos from DB

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,17 @@ class Todo < ActiveRecord::Base
     status = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:short)} #{todo_text} #{status}"
   end
+
+  def self.overdue
+    where("due_date < ?", Date.parse(Date.today.to_s))
+    #return Todo object array for due_date is less than Date.today"
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.parse(Date.today.to_s))
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.parse(Date.today.to_s))
+  end
 end
