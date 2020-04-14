af33eda698b005f9448376a11c6a657918b85b44
saipratap99 <122005067@sastra.ac.in>
2020-04-14 10:05:03 +0530

    Now application shows messages during signin,sinup,singout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,21 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:current_user_id] = user.id
+      flash[:notice] = "Your login is successful!"
       redirect_to todos_path
+    elsif user
+      flash[:alert] = "Invalid email and password"
+      redirect_to new_session_path
     else
-      render plain: "Invalid email and password"
+      flash[:alert] = "Account associated with #{params[:email]} not found"
+      redirect_to new_session_path
     end
   end
 
   def destroy
     session[:current_user_id] = nil
     @current_user = nil
+    flash[:notice] = "Your logged out is successfully!"
     redirect_to root_path
   end
 end
